63e63a5c97d10782f3d608c2f8d419701eaaf869
eazama <eazama001@gmail.com>
2017-05-25 19:53:28 -0700

    Broke core client functions into abstract classes

--- src/main/java/com/ryuusen/kafka/client/producer/core/CoreProducer.java
+++ src/main/java/com/ryuusen/kafka/client/producer/core/CoreProducer.java
@@ -1,47 +1,17 @@
 package com.ryuusen.kafka.client.producer.core;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
-import org.apache.kafka.common.PartitionInfo;
 
-public class CoreProducer<K, V> implements Producer<K, V>{
-
-	private Producer<K, V> primaryProducer;
+public class CoreProducer<K, V> extends AbstractProducer<K, V>{
 	
 	public CoreProducer(Properties props){
-		primaryProducer = new KafkaProducer<K, V>(props);
-	}
-	
-	@Deprecated
-	@Override
-	public Future<RecordMetadata> send(ProducerRecord<K, V> record) {
-		return this.sendAsync(record);
-	}
-	
-	@Deprecated
-	@Override
-	public Future<RecordMetadata> send(ProducerRecord<K, V> record, Callback callback) {
-		return this.sendAsync(record, callback);
-	}
-		
-	public Future<RecordMetadata> sendAsync(ProducerRecord<K, V> record) {
-		return this.sendAsync(record, null);
-	}
-
-	public Future<RecordMetadata> sendAsync(ProducerRecord<K, V> record, Callback callback) {
-		return primaryProducer.send(record, callback);
+		this.setPrimary(new KafkaProducer<K, V>(props));
 	}
 	
 	public Future<RecordMetadata> sendAsync(String topic, K key, V value) {
@@ -83,30 +53,4 @@ public class CoreProducer<K, V> implements Producer<K, V>{
 	public RecordMetadata sendSync(String topic, V value, Callback callback) throws InterruptedException, ExecutionException{
 		return this.sendSync(topic, null, value, callback);
 	}
-	
-	@Override
-	public void flush() {
-		primaryProducer.flush();
-	}
-
-	@Override
-	public List<PartitionInfo> partitionsFor(String topic) {
-		return primaryProducer.partitionsFor(topic);
-	}
-
-	@Override
-	public Map<MetricName, ? extends Metric> metrics() {
-		return primaryProducer.metrics();
-	}
-
-	@Override
-	public void close() {
-		primaryProducer.close();
-	}
-
-	@Override
-	public void close(long timeout, TimeUnit unit) {
-		primaryProducer.close(timeout, unit);
-	}
-
 }
